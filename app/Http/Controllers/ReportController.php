5ffe688a383e31b5fc1ca5294d9fe5043e7282df
lazlanrafar <lazlanrafar@gmail.com>
2024-06-11 15:31:06 +0800

    PUSH

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -20,21 +20,21 @@ class ReportController extends Controller
             $item->Downtime_Total = $dataPerMonth->sum('Downtime_Total');
             $item->Available_T = $dataPerMonth->sum('Available_T');
             $item->Operating_Time = $item->Available_T - $item->Downtime_Total;
-            $item->Available_Rate = number_format((($item->Operating_Time / $item->Available_T) * 100) / 100, 2);
+            $item->Available_Rate = number_format((($item->Operating_Time / $item->Available_T) * 100) / 100, 0);
 
             // Performance
             $item->Output = $dataPerMonth->sum('Output');
             $item->ActualCycleTime = $dataPerMonth->sum('ActualCycleTime');
             $item->PlanCycleTime = $dataPerMonth->sum('PlanCycleTime');
-            $item->Performance_Rate = number_format((($item->PlanCycleTime / $item->ActualCycleTime) * 100) / 100, 2);
+            $item->Performance_Rate = number_format((($item->PlanCycleTime / $item->ActualCycleTime) * 100) / 100, 0);
 
             // Quality
             $item->Reject_Total = $dataPerMonth->sum('Reject_Total');
             $item->Finish_Total = $item->Output - $item->Reject_Total;
-            $item->Quality_Rate = number_format($item->Output / ($item->Output + $item->Reject_Total), 2);
+            $item->Quality_Rate = number_format($item->Output / ($item->Output + $item->Reject_Total), 0);
 
             // OEE
-            $item->OEE = number_format($item->Available_Rate * $item->Performance_Rate * $item->Quality_Rate, 2);
+            $item->OEE = number_format($item->Available_Rate * $item->Performance_Rate * $item->Quality_Rate, 0);
         }
 
         return view('pages.report.index', [
